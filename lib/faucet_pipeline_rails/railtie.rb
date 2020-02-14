cec82305fd9fae6211c9964a8da189d815867e32
Dirk Breuer <mail@breuer.io>
2020-02-14 15:15:19 +0100

    Make sure to path `compute_asset_path` for all circumstances

--- lib/faucet_pipeline_rails/railtie.rb
+++ lib/faucet_pipeline_rails/railtie.rb
@@ -6,7 +6,7 @@ module FaucetPipelineRails
 
     ActiveSupport.on_load(:action_view) do
       # Overwrite `compute_asset_path` with our own implementation
-      define_method(:compute_asset_path) do |source, _|
+      ActionView::Helpers::AssetUrlHelper.define_method(:compute_asset_path) do |source, _|
         Manifest.instance.fetch(source)
       end
     end
